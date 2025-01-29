ec7de7e5f174968b01a40e34c0e35c046cce474a
mehdi <mahdi.fti79@gmail.com>
2025-01-29 16:13:42 +0330

    test 6 again player

--- components/pages/episode/VideoHeader.vue
+++ components/pages/episode/VideoHeader.vue
@@ -1,14 +1,26 @@
 <template>
   <ClientOnly>
-    <div class=" container video-header">
-      <Player :movie="movie" :video-url="videoUrl" :start-time="startTime" :no-container="noContainer"
-        :is-inside-video-series="isInsideVideoSeries" :on-pause="onPause" />
+    <div class="container video-header">
+      <Player 
+        :movie="movie" 
+        :video-url="videoUrl" 
+        :start-time="startTime" 
+        :no-container="noContainer"
+        :is-inside-video-series="true" 
+        :on-pause="onPause" 
+        ref="playerRef"
+      />
     </div>
   </ClientOnly>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 import Player from '~/components/core/Player.vue';
+import { useVideoPlayer } from '~/composables/useVideoPlayer';
+
+const playerRef = ref(null);
+const { registerPlayer, unregisterPlayer } = useVideoPlayer();
 
 const props = defineProps<{
   movie: {
@@ -27,10 +39,28 @@ const props = defineProps<{
   onPause?: (currentTime: number) => void;
   startTime?: number;
 }>();
+
+onMounted(() => {
+  if (playerRef.value) {
+    registerPlayer(playerRef.value);
+  }
+});
+
+onBeforeUnmount(() => {
+  if (playerRef.value) {
+    unregisterPlayer(playerRef.value);
+  }
+});
 </script>
 
 <style lang='scss' scoped>
 .video-header {
   border-radius: 5px;
+  width: 100%;
+  
+  :deep(.video-player) {
+    aspect-ratio: 16/9;
+    width: 100%;
+  }
 }
 </style>
